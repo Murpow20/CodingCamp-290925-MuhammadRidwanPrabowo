ecb7604e40dc7b1ecb6d064880b49b0f86bc3570
Murpow20 <ridwan20malik22@gmail.com>
2025-10-03 16:55:44 +0700

    Implement To-do List functionality with HTML structure, CSS styling, and JavaScript functions

---
+++ JavaScript/todoScript.js
@@ -0,0 +1,11 @@
+console.log("Hello, World!");
+
+function addTodo() {
+    const todoInput = document.getElementById("todo-input");
+    const dateInput = document.getElementById("date-input");
+    
+}
+
+function deleteAllTodo() {}
+
+function filterTodo() {}
